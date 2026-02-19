e1d0f726eb5ac1ac342bd0442cce6c6ae10ab8a4
anurag <anuragneupane58@gmail.com>
2026-02-20 01:39:48 +0545

    Fix: add @types/node for process env support

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-app.enableCors({
-  origin: '*',
-});
-const port = process.env.PORT || 3000;
-await app.listen(port);
+  app.enableCors({
+    origin: '*',
+  });
+  const port = 3000;
+  await app.listen(port);
 }
 bootstrap();
-//added
